cdc940b16b5b175cccbc4ccf6d41fcdebde9b65a
kter <me@kter.jp>
2017-10-06 23:50:27 +0900

    Initial Commit

---
+++ firstProduct/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  firstProduct
+//
+//  Created by 高橋智彦 on 2017/10/06.
+//  Copyright © 2017年 高橋智彦. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
